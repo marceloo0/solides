3085fc320ad9d4d5c01a5691cf46d134432d4174
Marcelo Barbosa <marceloppb@outlook.com>
2021-01-05 01:44:03 -0300

    solides frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000/api'
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.NEXT_APP_API_URL
 })
 
 export default api
